a41dc9736844847655503f2343ba6daaa0dc085b
DevSravani <111759556+DevSravani@users.noreply.github.com>
2024-02-20 12:02:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,81 @@
-provider "google"{
-    credentials = file("/home/sravanichedurupaku/creating_VM/flask-key.json")
-    project = "	digital-shelter-412213"
-    region  = "us-central1"
-    zone    = "us-central1-a"
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
 }
-resource "google_compute_instance "vminstance" {
-    machine_type = "e2-medium"
-    region       = "us-central1"
-    zone         = "us-central1-a"
 
-    boot_disk{
-      initialize_params {
-      name      = "vminstance"
-      type      = "new-balanced-persistent-disk"
-      size      = "10"
-      image     = "My First Project/debian-cloud/global/images/debian-10-20240110"
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "e2-medium"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image      = "debian-cloud/debian-11"
+    auto_delete       = true
+    boot              = true
+    // backup the disk every day
+    resource_policies = [google_compute_resource_policy.daily_backup.id]
+  }
+
+  // Use an existing disk resource
+  disk {
+    // Instance Templates reference disks by name, not self link
+    source      = google_compute_disk.foobar.name
+    auto_delete = false
+    boot        = false
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
   }
 }
-network_interface {
-    network       = "default"
-    access_config = {}  
+
+data "google_compute_image" "my_image" {
+  family  = "debian-11"
+  project = "debian-cloud"
+}
+
+resource "google_compute_disk" "foobar" {
+  name  = "existing-disk"
+  image = data.google_compute_image.my_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+resource "google_compute_resource_policy" "daily_backup" {
+  name   = "every-day-4am"
+  region = "us-central1"
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "04:00"
+      }
+    }
   }
+}
