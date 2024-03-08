c004982ec212e509b9e2a37187a368af57a09850
DevSravani <111759556+DevSravani@users.noreply.github.com>
2024-03-08 13:07:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,3 +14,19 @@ provider "google" {
   zone    ="us-cental1-a"
   credentials = "./key.json"
 }
+resource "google_storage_bucket" "f0001" {
+  name     = "f0001" # Specify your desired bucket name
+  location = "US-central1"
+  project  = "learning-project-414414"
+
+
+
+  # Optionally, enable versioning for the bucket
+  versioning {
+    enabled = true
+  }
+
+  # Optionally, set the bucket's access control
+  #iam_configuration {
+  #bucket_policy_only = true
+}
